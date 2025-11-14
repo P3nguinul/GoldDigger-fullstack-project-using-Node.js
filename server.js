6a7762339515759f5e3d0498dfe25c9b9bb09376
Ianache Ionut <ianacheionut10@gmail.com>
2025-11-14 19:31:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 import http from 'node:http'
 import { serveFiles } from './utils/serveFiles.js'
+import { getGoldPrice } from './utils/getGoldPrice.js'
+import { setSSEheaders } from './utils/setSSEheaders.js'
+import { handlePost } from './utils/handlePost.js'
 
 const PORT = 8001
 const __dirname = import.meta.dirname
-console.log(typeof __dirname)
 
 const server = http.createServer(async (req, res) => {
 
-if( req.url === '/api') {
+if ( req.url === '/sse') {
+  setSSEheaders(res)
+  setInterval(() => {
+    const  goldPrice = getGoldPrice()    
 
-  res.end('hi')
+    res.write(`data:${JSON.stringify({event: 'update-price', price: goldPrice})}\n\n`)
+    
+  }, 3000)
 
-} else if ( !req.url.startsWith('/api')) {
+} else  if (req.url === '/log' && req.method === 'POST') {
+
+    handlePost(req,res)    
+
+  } else {
 
   return await serveFiles(req,res, __dirname)
 
-}
+  }
 
 })
 
