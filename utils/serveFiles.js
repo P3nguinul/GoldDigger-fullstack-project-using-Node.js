7817890455ddd67e24a54f9d6f8aaed312030241
Ianache Ionut <ianacheionut10@gmail.com>
2025-11-14 19:33:09 +0200

    Update serveFiles.js

--- utils/serveFiles.js
+++ utils/serveFiles.js
@@ -8,17 +8,15 @@ export async  function serveFiles(req, res, dirname) {
 
   const publicPath = path.join(dirname, 'public')
   const filePath = path.join(publicPath, req.url === '/' ? 'index.html' : req.url)
-  console.log(filePath)
   const fileExtension = path.extname(filePath)
-  console.log(fileExtension)
   const extension = getContentType(fileExtension)
-  console.log(extension)
   
   try {
   const content = await fs.readFile(filePath)
   sendResponse(res, 200, extension , content)
   } catch (err) {
-    sendResponse(res,400, 'index.html', err)
+    const content = await fs.readFile(path.join(publicPath, '404.html'))
+    sendResponse(res,400, 'index.html', content)
   }
 
 }
